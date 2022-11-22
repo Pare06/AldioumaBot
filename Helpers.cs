2f996c8115ae93fe2b33231d62eceffe52bc90aa
Jacopo <Jacopo@JACOPO-PC>
2022-11-22 20:59:30 +0100

    merda

--- Helpers.cs
+++ Helpers.cs
@@ -7,5 +7,11 @@
 		{
 			return "⠀⠀⠀⠀   ⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠤⠤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀\r\n    ⠀⠀⠀⠀⠀⢀⣾⣟⠳⢦⡀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠉⠉⠉⠉⠉⠒⣲⡄\r\n    ⠀⠀⠀⠀⠀⣿⣿⣿⡇⡇⡱⠲⢤⣀⠀⠀⠀⢸⠀⠀⠀1984⠀⣠⠴⠊⢹⠁\r\n    ⠀⠀⠀⠀⠀⠘⢻⠓⠀⠉⣥⣀⣠⠞⠀⠀⠀⢸⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⢸⠀\r\n    ⠀⠀⠀⠀⢀⣀⡾⣄⠀⠀⢳⠀⠀⠀⠀⠀⠀⢸⢠⡄⢀⡴⠁⠀⠀⠀⠀⠀⡞⠀\r\n    ⠀⠀⠀⣠⢎⡉⢦⡀⠀⠀⡸⠀⠀⠀⠀⠀⢀⡼⣣⠧⡼⠀⠀⠀⠀⠀⠀⢠⠇⠀\r\n    ⠀⢀⡔⠁⠀⠙⠢⢭⣢⡚⢣⠀⠀⠀⠀⠀⢀⣇⠁⢸⠁⠀⠀⠀⠀⠀⠀⢸⠀⠀\r\n    ⠀⡞⠀⠀⠀⠀⠀⠀⠈⢫⡉⠀⠀⠀⠀⢠⢮⠈⡦⠋⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀\r\n    ⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⣀⡴⠃⠀⡷⡇⢀⡴⠋⠉⠉⠙⠓⠒⠃⠀⠀\r\n    ⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⡼⠀⣷⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n    ⡞⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⠀⡰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n    ⢧⠀⠀⠀⠀⠀⠀⠀⠈⠣⣀⠀⠀⡰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
 		}
+
+#pragma warning disable CS8618
+		public static string[] KeyWords { get; set; }
+
+		public static void SetKeyWords()
+			=> KeyWords = new string[] { "duomo", "@everyone", "kebe", "oggettiv", "bersaglio", "bombe", "attentato", "allah", "kabobo", "matteo", "1984", "avigliano", "cyberpunk", "aiuto" };
 	}
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ using Discord.Commands;
 using Discord.Net;
 using Discord.Rest;
 using Discord.WebSocket;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
+using static AldioumaBot.Helpers;
 
 namespace AldioumaBot
 {
@@ -22,24 +25,45 @@ namespace AldioumaBot
 		public static DiscordSocketClient Client { get; set; }
 #pragma warning restore
 
+		#region constants
+		// windows api
+		public const int WINDOW_HIDE = 0;
+		public const int WINDOW_SHOW = 5;
+		// guild ids
 		public const ulong DlscordBase = 786009481246277653;
 		public const ulong Crozza = 1040658160689631302;
 		public const ulong TestServer = 1017448373776814120;
+		// user ids
+		public const ulong PareId = 566905651310362656;
+		public const ulong SpaceId = 920346045135872020;
+		public const ulong AldioumaId = 1016759379028095047;
+		public const ulong VanniBotId = 867526392156258324;
+		// channel ids
+		public const ulong WakeUpId = 1016799894335410187;
+        // role ids
+        public static IRole? Presidente { get; set; }
+		public static IRole? STwitch { get; set; }
+		// HandleChatMessage
+		public readonly static DateTime Attentato = new(2023, 8, 16, 15, 32, 00);
+		public delegate Task Command();
+		// altra merda
+		public const string ResourcePath = @"C:\Users\Jacopo\Desktop\aldiouma\";
+        #endregion // constants
 
-		public delegate Task Command(); // per GetCommands()
-
-		public static async Task Main()
+        public static async Task Main()
 		{
 			AllocConsole();
-			ShowWindow(GetConsoleWindow(), 0);
-
+			ShowWindow(GetConsoleWindow(), WINDOW_HIDE);
+			Console.OutputEncoding = Encoding.Unicode;
+			SetKeyWords();
+			
 			Client = new();
 			await Client.LoginAsync(TokenType.Bot, Token.DiscordToken);
 			await Client.StartAsync();
 
 			Client.Ready += SetUpBot;
 
-			await Task.Delay(-1);
+			await Task.Delay(Timeout.Infinite);
 		}
 
 		private static async Task SetUpBot()
@@ -48,13 +72,27 @@ namespace AldioumaBot
 			Client.UserVoiceStateUpdated += HandleUserVoiceState; // quando qualcuno in vocale viene inculato
 			Client.SlashCommandExecuted += HandleSlashCommand; // quando viene eseguito /summon
 
+			#region static setters
+			Presidente = Client.GetGuild(DlscordBase).GetRole(786009704270004228);
+			STwitch = Client.GetGuild(DlscordBase).GetRole(1040741716547866704);
+			#endregion
+
+			#region command builders
 			SlashCommandBuilder summon = new();
 			summon.WithName("summon")
 				.WithDescription("per quando qualcuno ha l'ebola")
 				.WithDMPermission(false)
-				.AddOption("cancro", ApplicationCommandOptionType.User, "il cancro da evocare");
+				.AddOption("cancro", ApplicationCommandOptionType.User, "il cancro da evocare", true);
+
+			SlashCommandBuilder rating = new();
+			rating.WithName("rating")
+				.WithDescription("fa il cock rating")
+				.WithDMPermission(false)
+				.AddOption("pisello", ApplicationCommandOptionType.User, "persona a cui fare il cock rating", true);
+			#endregion
 
 			await Client.CreateGlobalApplicationCommandAsync(summon.Build());
+			await Client.CreateGlobalApplicationCommandAsync(rating.Build());
 		}
 
 		private static async Task HandleChatMessage(SocketMessage arg)
@@ -65,17 +103,18 @@ namespace AldioumaBot
 			if (message.Author.IsBot) return;
 			if (context.IsPrivate)
 			{
-				if (context.User.Id == 566905651310362656)
+				if (context.User.Id == PareId)
 				{
 					switch (message.Content)
 					{
 						case "show":
-							ShowWindow(GetConsoleWindow(), 5);
+							ShowWindow(GetConsoleWindow(), WINDOW_SHOW);
 
 							#region CODICE DISUMANO DA NON GUARDARE SE NON PER QUANDO C'È DA FUCILARE GLI IRACHENI
 #pragma warning disable CS8629
 							string msg = string.Empty;
-							await foreach (var auditLogs in Client.GetGuild(786009481246277653).GetAuditLogsAsync(100))
+							ulong guild = Crozza;
+							await foreach (var auditLogs in Client.GetGuild(guild).GetAuditLogsAsync(100))
 							{
 								foreach (var audit in auditLogs)
 								{
@@ -107,7 +146,7 @@ namespace AldioumaBot
 										case ActionType.RoleUpdated:
 											var before = ((RoleUpdateAuditLogData)audit.Data).Before;
 											var after = ((RoleUpdateAuditLogData)audit.Data).After;
-											data += Client.GetGuild(786009481246277653).GetRole(((RoleUpdateAuditLogData)audit.Data).RoleId).Name;
+											data += Client.GetGuild(guild).GetRole(((RoleUpdateAuditLogData)audit.Data).RoleId).Name;
 											if (((GuildPermissions)before.Permissions).Administrator && !((GuildPermissions)after.Permissions).Administrator)
 											{
 												data += " -ADMIN";
@@ -133,7 +172,7 @@ namespace AldioumaBot
 							break;
 						case "hide":
 							Console.Clear();
-							ShowWindow(GetConsoleWindow(), 0);
+							ShowWindow(GetConsoleWindow(), WINDOW_HIDE);
 							break;
 						default:
 							break;
@@ -144,32 +183,12 @@ namespace AldioumaBot
 
 			SocketTextChannel ch = (SocketTextChannel)message.Channel;
 
-			for (int i = 0; i < GetKeywords().Length; i++)
+			for (int i = 0; i < KeyWords.Length; i++)
 			{
-				if (message.Content.Contains(GetKeywords()[i]))
+				if (message.Content.Contains(KeyWords[i]))
 				{
 					await GetCommands()[i]();
 				}
-			}
-
-			static string[] GetKeywords()
-			{
-				return new string[]
-				{
-					"duomo",
-					"@everyone",
-					"kebe",
-					"oggettiv",
-					"bersaglio",
-					"bombe",
-					"attentato",
-					"allah",
-					"kabobo",
-					"matteo",
-					"1984",
-					"avigliano",
-					"cyberpunk"
-				};
 			}
 
 			Command[] GetCommands()
@@ -178,26 +197,39 @@ namespace AldioumaBot
 				{
 					async () =>
 					{
-						DateTime attentato = new(2023, 8, 16, 15, 32, 00);
-						if (DateTime.Now > attentato) // dopo il 16/8?
-							await ch.SendMessageAsync($"il duomo è gia esploso {Math.Floor((DateTime.Now - attentato).TotalDays)} giorni fa 🙏🏿");
-						else if (DateTime.Now < attentato) // prima del 16/8?
-							await ch.SendMessageAsync($"mancano {Math.Floor((DateTime.Now - attentato).TotalDays * -1)} giorni all'attentato al duomo di pisa 🙏🏿");
+						double timeDiff = (DateTime.Now - Attentato).TotalDays;
+						if (DateTime.Now > Attentato) // dopo il 16/8?
+							await ch.SendMessageAsync($"il duomo è gia esploso {Math.Floor(timeDiff)} giorni fa 🙏🏿");
+						else if (DateTime.Now < Attentato) // prima del 16/8?
+							await ch.SendMessageAsync($"mancano {Math.Floor(timeDiff * -1)} giorni all'attentato al duomo di pisa 🙏🏿");
 						else // è il 16/8?
 							await ch.SendMessageAsync("oggi è il grande giorno 🙏🏿 preparate le bombe");
 					},
 					async () => await ch.SendMessageAsync("dio stronzo mi sto a segà e questo tagga tutti"),
 					async () => await ch.SendMessageAsync("🙏🏿 goudi kebe akbar mihail touba sawarim islam jihad pontedera allah aldiouma 🙏🏿"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/eLS0Flt"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/XIMVXEg"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/QcmIdN0"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/gHX8tVP"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/k810KJc"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/zMIeZ9N"),
-					async () => await ch.SendMessageAsync("https://imgur.com/a/umENPRL"),
-					async () => await ch.SendMessageAsync(Helpers.Get1984()),
+					async () => await ch.SendFileAsync($"{ResourcePath}oggettività.jpeg", ""),
+					async () => await ch.SendFileAsync($"{ResourcePath}duomo.png", ""),
+					async () => await ch.SendFileAsync($"{ResourcePath}bombe.jpeg", ""),
+					async () => await ch.SendFileAsync($"{ResourcePath}beirut.gif", ""),
+					async () => await ch.SendFileAsync($"{ResourcePath}kebe isis.png", ""),
+					async () => await ch.SendFileAsync($"{ResourcePath}senegalese.jpeg", ""),
+					async () => await ch.SendFileAsync($"{ResourcePath}sbrana duce.png", ""),
+					async () => await ch.SendMessageAsync(Get1984()),
 					async () => await ch.SendMessageAsync("è arrivata mammina la piscia r ta mamma scrivi scrivi :grin: si sol n'abbunat"),
-					async () => await ch.SendMessageAsync("https://www.youtube.com/watch?v=qUxBn5KDxsA ووكدسكاكپوسداوكيدجخجفدسفيس")
+					async () => await ch.SendMessageAsync("https://www.youtube.com/watch?v=qUxBn5KDxsA ووكدسكاكپوسداوكيدجخجفدسفيس"),
+					async () =>
+					{
+						string msg = string.Empty;
+						msg += "comandi:\n/summon\n/rating\n\n";
+						msg += "parole sus: ";
+						foreach (string cmd in KeyWords)
+						{
+							if (cmd == "aiuto") continue;
+							msg += $"{cmd}, ";
+						}
+						msg = msg.Replace("@", "@|").Remove(msg.Length - 1);
+						await ch.SendMessageAsync(msg);
+					}
 				};
 			}
 		}
@@ -211,43 +243,38 @@ namespace AldioumaBot
 				|| before.VoiceChannel is null // join
 				|| after.VoiceChannel is null // leave
 				|| (before.VoiceChannel.Id == after.VoiceChannel.Id) // non si è spostato
-				|| (before.VoiceChannel.Guild.Id != 786009481246277653) // non in dlscord base
+				|| (before.VoiceChannel.Guild.Id != DlscordBase) // non in dlscord base
 				) return;
 
-				var channel = Client.GetGuild(DlscordBase).GetTextChannel(1016799894335410187);
+				var channel = Client.GetGuild(DlscordBase).GetTextChannel(WakeUpId);
 
 				EmbedBuilder embed = new();
 				embed.WithAuthor("MUHAMED KEBE")
 					.WithColor(Discord.Color.Blue)
-					.WithImageUrl("https://i.imgur.com/CDUriO4.jpg")
-					.WithThumbnailUrl("https://i.imgur.com/CylNIQY.png")
+					.WithImageUrl($"attachment://{ResourcePath}wake up.jpg")
+					.WithThumbnailUrl($"attachment://{ResourcePath}corti.png")
 					.WithFooter("A MENO CHE TU NON SIA IN COMA VEDI DI ENTRARE IN UN CANALE");
 
 				await channel.SendMessageAsync($"{user.Mention} WAKE UP  WAKE UP  WAKE UP  WAKE UP  WAKE UP  WAKE UP  WAKE UP", embed: embed.Build());
 			}
-			catch
-			{
-				// solo quando è in corso HandleSlashCommand(), oppure 1 volta su 500
-
-				// https://it.wikipedia.org/wiki/Algoritmo_dello_struzzo
-			}
+			catch { } // se è eseguito /summon
 		}
 
 		private static async Task HandleSlashCommand(SocketSlashCommand command)
 		{
 			if (command.CommandName == "summon")
 			{
-				if (command.User.Id == 920346045135872020) // bambino piccolo che di cognome fa appiani?
+				if (command.User.Id == SpaceId) // bambino piccolo che di cognome fa appiani?
 				{
 					await command.RespondAsync("dio porco ti chiami francesco appiani e pensi di meritarti qualcosa da un bot");
 					return;
 				}
-				else if (((SocketGuildUser)command.User).Guild.Id == DlscordBase && !((SocketGuildUser)command.User).Roles.Contains(Client.GetGuild(DlscordBase).GetRole(786009704270004228))) // dlscord | presidente?
+				else if (((SocketGuildUser)command.User).Guild.Id == DlscordBase && !((SocketGuildUser)command.User).Roles.Contains(Presidente)) // dlscord | presidente?
 				{
 					await command.RespondAsync("non hai il ruolo presidente quindi non conti un cazzo");
 					return;
 				}
-				else if (((SocketGuildUser)command.User).Guild.Id == Crozza && !((SocketGuildUser)command.User).Roles.Contains(Client.GetGuild(DlscordBase).GetRole(1040741716547866704))) // crozza | twitch?
+				else if (((SocketGuildUser)command.User).Guild.Id == Crozza && ((SocketGuildUser)command.User).Roles.Contains(STwitch)) // crozza | twitch?
 				{
 					await command.RespondAsync("non hai il ruolo signor. twitch quindi non conti un cazzo");
 					return;
@@ -257,14 +284,20 @@ namespace AldioumaBot
 				string name = cancro.Username;
 				string discriminator = cancro.Discriminator;
 
+				if (cancro.Id == command.User.Id)
+				{
+					await command.RespondAsync("porco dio sei disabile come francesco paolo farina");
+					return;	
+				}
+				if (cancro.Id == AldioumaId || cancro.Id == PareId)
+				{
+					await command.RespondAsync($"ti ammazzo nel sonno");
+					await command.Channel.SendFileAsync($"{ResourcePath}mihai ferro.png", "");
+					return;
+				}
 				if (cancro.VoiceChannel is null)
 				{
 					await command.RespondAsync("dio negro non è in chat vocale");
-					return;
-				}
-				if (cancro.Id == 1016759379028095047)
-				{
-					await command.RespondAsync("https://imgur.com/a/nKG1yGp");
 					return;
 				}
 
@@ -282,6 +315,28 @@ namespace AldioumaBot
 					catch (HttpException) { } // carcere di twitter
 				}
 				await cancro.ModifyAsync(user => user.Channel = channel);
+			}
+			
+			else if (command.CommandName == "rating")
+			{
+				SocketGuildUser user = (SocketGuildUser)command.Data.Options.ElementAt(0).Value;
+				ISocketMessageChannel channel = command.Channel;
+
+				await command.RespondAsync($"il cazzo e le palle di {user.Mention} ricevono il voto di...");
+
+				switch (user.Id)
+				{
+					case AldioumaId: // aldiouma
+						await channel.SendMessageAsync("10/10. sono bellissimo e ho tutte le fighe.");
+						return;
+					case VanniBotId: // vanni
+						await channel.SendMessageAsync("-1/10. LUI È IL MIO ACERRIMO NEMICO. FA SCHIFO.");
+						return;
+					default:
+						ulong vote = user.Id % 11;
+						await channel.SendMessageAsync($"{vote}/10. {(vote == 0 ? "le palle sembrano uscite dal benin." : "")} {((vote == 10) ? "bellissime palle." : "")}");
+						return;
+				}
 			}
 		}
 	}
